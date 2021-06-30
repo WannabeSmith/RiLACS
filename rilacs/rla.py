2e6e5ec1cad659569111791f05c2ac8272a3f27f
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2021-06-30 18:28:26 -0400

    Add type suggestions for rla.py

--- rilacs/rla.py
+++ rilacs/rla.py
@@ -1,14 +1,15 @@
+from typing import Callable
 import numpy as np
+import numpy.typing as npt
 from scipy.optimize import root
 from confseq.betting import (
     betting_mart,
     diversified_betting_mart,
-    cs_from_martingale,
     mu_t,
 )
 
 
-def apriori_Kelly_bet_general(x_reported):
+def apriori_Kelly_bet_general(x_reported: npt.ArrayLike[float]) -> float:
     objective = lambda l: np.sum((x_reported - 0.5) / (1 + l * (x_reported - 0.5)))
 
     sol = root(objective, x0=0.1)
@@ -17,11 +18,13 @@ def apriori_Kelly_bet_general(x_reported):
     return bet
 
 
-def apriori_Kelly_bet(n_A, n_B):
+def apriori_Kelly_bet(n_A: int, n_B: int) -> float:
     return 2 * (n_A - n_B) / (n_A + n_B)
 
 
-def apriori_Kelly_martingale(x, m, N, n_A, n_B):
+def apriori_Kelly_martingale(
+    x: npt.ArrayLike[float], m: float, N: int, n_A: int, n_B: int
+) -> npt.ArrayLike[float]:
     apriori_Kelly_lambda = lambda x, m: apriori_Kelly_bet(n_A, n_B)
     return betting_mart(
         x,
@@ -35,7 +38,14 @@ def apriori_Kelly_martingale(x, m, N, n_A, n_B):
     )
 
 
-def distKelly_martingale(x, m, N, dist, D=10, beta=1):
+def distKelly_martingale(
+    x: npt.ArrayLike[float],
+    m: float,
+    N: int,
+    dist: Callable[[npt.ArrayLike[float]], npt.ArrayLike[float]],
+    D: int = 10,
+    beta: float = 1,
+):
     lambdas_fns_positive = [
         lambda x, m, i=i: (i + 1) / (mu_t(x, m, N) * (D + 1)) for i in range(D)
     ]
@@ -64,9 +74,9 @@ def distKelly_martingale(x, m, N, dist, D=10, beta=1):
     return mart
 
 
-def linear_gamma_dist(y):
+def linear_gamma_dist(y: npt.ArrayLike[float]):
     return np.maximum(1 - 4 * y, 0)
 
 
-def square_gamma_dist(y):
+def square_gamma_dist(y: npt.ArrayLike[float]):
     return (y <= 1 / 3) * (1 / 3 - y) ** 2
