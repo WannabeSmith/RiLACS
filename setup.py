a9a03afb03fc01a877b229214d69bccd6a8652f8
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2023-05-18 02:09:48 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="rilacs",
-    version="1.0.1",
+    version="1.0.2",
     description="Risk limiting audits via confidence sequences",
     long_description=long_description,
     url="http://github.com/WannabeSmith/RiLACS",
